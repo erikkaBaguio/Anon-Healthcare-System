638f8c0fe547fc168583acdf5219a3705f878630
erikkaBaguio <erikka.baguio@gmail.com>
2016-02-18 02:18:08 +0800

    Added Doctor_referral_steps.py

---
+++ Code Model/tests/bdd/steps/Doctor_Referral_steps.py
@@ -0,0 +1,14 @@
+from lettuce import step
+from nose.tools import *
+
+@step(u'Given the doctor has already approved the appointment request.')
+def given_the_doctor_has_already_approved_the_appointment_request(step):
+    assert False, 'This step must be implemented'
+
+@step(u'When the doctor refers the patient to another doctor.')
+def when_the_doctor_refers_the_patient_to_another_doctor(step):
+    assert False, 'This step must be implemented'
+
+@step(u'Then the other doctor receives the appointment request.')
+def then_the_other_doctor_receives_the_appointment_request(step):
+    assert False, 'This step must be implemented'
