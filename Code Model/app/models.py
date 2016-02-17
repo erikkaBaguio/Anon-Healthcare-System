4866b21ef937f7dad8db884f763bcdc2d1d25949
marygracecabolbol <marygrace.cabolbol@g.msuiit.edu.ph>
2016-02-18 03:13:09 +0800

    added User model class

--- Code Model/app/models.py
+++ Code Model/app/models.py
@@ -1,9 +1,28 @@
 from app import db
-class Post(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(128))
-  body = db.Column(db.Text)
 
-  def __init__(self, title, body):
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
+    body = db.Column(db.Text)
+
+    def __init__(self, title, body):
         self.title = title
         self.body = body
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    f_name = db.Column(db.String(45))
+    m_name = db.Column(db.String(45))
+    l_name = db.Column(db.String(45))
+    role = db.Column(db.Integer)  # 1-admin, 2-doctor, 3-nurse, 4-patient
+    username = db.Column(db.String(50), unique=True, index=True)
+    password = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True, index=True)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
