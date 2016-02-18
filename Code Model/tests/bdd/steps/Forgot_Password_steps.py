1b088633693af284afd3aefe6e17a83d0449d13b
JosiahRegencia <jawshaeleazar@gmail.com>
2016-02-18 10:20:44 +0800

    ADDED Forgot_Password_steps.py

---
+++ Code Model/tests/bdd/steps/Forgot_Password_steps.py
@@ -0,0 +1,26 @@
+from lettuce import step
+
+
+@step(u'Given admin/doctor/nurse has already clicked forgot password button')
+def given_admin_doctor_nurse_has_already_clicked_forgot_password_button(step):
+    assert False, 'This step must be implemented'
+
+@step(u'And admin/doctor/nurse is already redirected to forgot password page')
+def and_admin_doctor_nurse_is_already_redirected_to_forgot_password_page(step):
+    assert False, 'This step must be implemented'
+
+@step(u'When admin/doctor/nurse inputs his/her email address used in the account')
+def when_admin_doctor_nurse_inputs_his_her_email_address_used_in_the_account(step):
+    assert False, 'This step must be implemented'
+
+@step(u'And admin/doctor/nurse clicks send email button')
+def and_admin_doctor_nurse_clicks_send_email_button(step):
+    assert False, 'This step must be implemented'
+
+@step(u'Then a link to inputting a new password will be sent to the email address of the admin/doctor/nurse')
+def then_a_link_to_inputting_a_new_password_will_be_sent_to_the_email_address_of_the_admin_doctor_nurse(step):
+    assert False, 'This step must be implemented'
+
+@step(u'And admin/doctor/nurse can change his/her password by clicking the link')
+def and_admin_doctor_nurse_can_change_his_her_password_by_clicking_the_link(step):
+    assert False, 'This step must be implemented'
