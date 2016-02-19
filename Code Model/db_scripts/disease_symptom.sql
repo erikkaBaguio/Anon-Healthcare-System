b01b25ad8fe1b97b4ca0d77df43152e38a4079e1
erikkaBaguio <erikka.baguio@gmail.com>
2016-02-20 01:55:08 +0800

    Added a stored procedure for inserting new disease data.

--- Code Model/db_scripts/disease_symptom.sql
+++ Code Model/db_scripts/disease_symptom.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Disease_Symptom(
-  id INT PRIMARY KEY,
-  disease_id INT REFERENCES Disease(id),
-  symptom_id INT REFERENCES Symptom(id),
-  user_id INT REFERENCES User(id),
+  id int primary key,
+  disease_id int references Disease(id),
+  symptom_id int references Symptom(id),
+  user_id int references User(id),
 );
