9946896990ab89b4a13fe856472eaa27cfc0b6fb
marygracecabolbol <marygrace.cabolbol@g.msuiit.edu.ph>
2016-02-19 19:36:24 +0800

    added table for Question_answer

--- Code Model/question.sql
+++ Code Model/question.sql
@@ -1,7 +1,7 @@
-create table question(
-    id primary key,
+create table Question(
+    id INT primary key,
     question string(200),
-    category_id FOREIGN KEY,
-    user_id FOREIGN KEY
+    user_id INT FOREIGN KEY REFERENCES User(id),
+    category_id INT FOREIGN KEY REFERENCES Question_category(id)
 );
 
