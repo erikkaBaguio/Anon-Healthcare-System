3e8cda689f1278dbd797eae8236dd3ae7b2e5019
marygracecabolbol <marygrace.cabolbol@g.msuiit.edu.ph>
2016-02-20 09:11:29 +0800

    added a stored proc in question_answer.sql for inserting new data

--- Code Model/db_scripts/question_answer.sql
+++ Code Model/db_scripts/question_answer.sql
@@ -1,7 +1,27 @@
 create table Question_answer (
-    id INT PRIMARY KEY,
-    question_id INT FOREIGN KEY REFERENCES Question(id),
-    answer_id INT FOREIGN KEY REFERENCES  Answer(id),
-    chained_to_question INT FOREIGN KEY REFERENCES  Question(question)
+  id                    INT PRIMARY KEY,
+  question_id           INT FOREIGN KEY REFERENCES Question(id),
+  answer_id             INT FOREIGN KEY REFERENCES  Answer(id),
+  chained_to_question   INT FOREIGN KEY REFERENCES  Question(question),
+  done                  boolean
 );
+
+CREATE or replace function newquestion_answer(par_id int, par_question_id int, par_answer_id int, par_chained_to_question int, done boolean) returns text as
+$$
+  declare
+    loc_id text;
+    loc_res text;
+  begin
+     select into loc_id id from Question_answer where id = par_id;
+     if loc_id isnull then
+       insert into Question_answer (id, question_id, answer_id, chained_to_question, done) values (par_id, par_question_id, par_done);
+       loc_res = 'OK';
+
+     else
+       loc_res = 'ID EXISTED';
+     end if;
+     return loc_res;
+  end;
+$$
+ language 'plpgsql';
 
