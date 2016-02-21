42d98c3b4677364143919017743c4c82456f428f
remarcbalisi <remarcbalisi@gmail.com>
2016-02-21 22:57:55 +0800

    Added Send_examination.py

---
+++ Code Model/tests/bdd/steps/Send_Examination.py
@@ -0,0 +1,21 @@
+from lettuce import step
+
+
+@step(u'The nurse sends examination answers to the doctor')
+def the_nurse_sends_examination_answers_to_the_doctor(step):
+    assert False, 'This step must be implemented'
+
+
+@step(u'Given The doctor approved the appointment request')
+def given_the_doctor_approved_the_appointment_request(step):
+    assert False, 'This step must be implemented'
+
+
+@step(u'When The nurse clicks the send button')
+def when_the_nurse_clicks_the_send_button(step):
+    assert False, 'This step must be implemented'
+
+
+@step(u'Then The doctor receives the examination answers.')
+def then_the_doctor_receives_the_examination_answers(step):
+    assert False, 'This step must be implemented'
