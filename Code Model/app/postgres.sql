48b5a377cb7bf44e0d74119f4a21b85f9b4cdfcb
JosiahRegencia <jawshaeleazar@gmail.com>
2016-02-25 03:39:16 +0800

    Fixed postgres.sql bugs

--- Code Model/app/postgres.sql
+++ Code Model/app/postgres.sql
@@ -5,11 +5,12 @@ create table UserInfo (
 	lname text,
 	email text,
 	password text,
+	role int,
 	is_active boolean
 );
 
 create or replace function newuserinfo(par_id int, par_fname text, par_mname text, par_lname text,
-                                par_email text, par_password text, par_is_active boolean) returns text
+                                par_email text, par_password text, par_role int, par_is_active boolean) returns text
                                 as
 $$
     declare
@@ -20,8 +21,8 @@ $$
         select into loc_id id from UserInfo where id = par_id;
         if loc_id isnull then
 
-            insert into UserInfo(id, fname, mname, lname, email, password, is_active) values
-                     (par_id, par_fname, par_mname, par_lname, par_email, par_password, par_is_active);
+            insert into UserInfo(id, fname, mname, lname, email, password, role, is_active) values
+                 (par_id, par_fname, par_mname, par_lname, par_email, par_password, par_role, par_is_active);
             loc_res = 'OK';
         else
             loc_res = 'ID EXISTED';
@@ -31,32 +32,24 @@ $$
 $$
   language 'plpgsql';
 
-create or replace function getuserinfo(out int, out text, out text, out text, out text,out boolean)
+create or replace function getuserinfo(out int, out text, out text, out text, out text, out int, out boolean)
                                             returns setof record as
 $$
-    select id, fname, mname, lname, email, is_active from UserInfo;
+    select id, fname, mname, lname, email, role, is_active from UserInfo;
 $$
   language 'sql';
 
 create or replace function getuserinfoid(in par_id int, out text, out text, out text, out text,
-                                                 out boolean) returns setof record as
+                                                 out int, out boolean) returns setof record as
 $$
-    select fname, mname, lname, email, is_active from UserInfo where par_id = id;
+    select fname, mname, lname, email, role, is_active from UserInfo where par_id = id;
 $$
   language 'sql';
 ----------------------------------------------------------------------------------------------------
 
 create table Symptom(
   id int primary key,
-  symptom text(200)
-);
-
-	id INT PRIMARY KEY,
-	fname varchar(45),
-	mname varchar(45),
-	lname varchar(45),
-	email varchar(45),
-	is_active boolean
+  symptom text
 );
 -----------------------------------------------------------------------------------------------------
 create table Schedule(
@@ -65,7 +58,7 @@ create table Schedule(
   done boolean
 );
 
-create or replace function newschedule(par_id int, par_date_time_year date, par_done BOOLEAN) return text as
+create or replace function newschedule(par_id int, par_date_time_year date, par_done BOOLEAN) returns text as
 $$
     declare
         loc_id text;
@@ -79,8 +72,8 @@ $$
 
         else
             loc_res = 'ID EXISTED';
-        end if
-        return loc_res
+        end if;
+        return loc_res;
     end;
 $$
     language 'plpgsql';
@@ -92,7 +85,7 @@ create table Examination (
   schedule_id int references Schedule(id),
   question_id int references Question(id),
   answer_id int references Answer(id),
-  examination_name varchar(200)
+  examination_name varchar(200),
   done BOOLEAN
 );
 -----------------------------------------------------------------------------------------------------------
@@ -110,11 +103,11 @@ create table Disease(
 create or replace function newdisease(par_id int, par_name varchar, par_done boolean) returns text as
 $$
   declare
-    loc_id text,
-    loc_res text,
+    loc_id text;
+    loc_res text;
   begin
     select into loc_id id from disease where id = par_id;
-    if loc_id id isnull then
+    if loc_id isnull then
 
       insert into disease(id, name, done) values (par_id, pr_name, par_done);
       loc_res = "New disease data is added.";
@@ -133,13 +126,6 @@ $$
 $$
   language 'sql'
 
-----------------------------------------------------------------------------------------------------
-
-create table Symptom(
-  id int primary key,
-  symptom text
-);
-
 ------------------------------------------------------------------------------------------------------------
 create table Disease_Symptom(
   id int primary key,
@@ -147,21 +133,10 @@ create table Disease_Symptom(
   symptom_id int references Symptom(id),
   user_id int references UserInfo(id)
 );
-------------------------------------------------------------------------------------------------------------
-create table Examination (
-  id int primary key,
-  user_id int references User(id),
-  schedule_id int references Schedule(id),
-  question_id int references Question(id),
-  answer_id int references Answer(id),
-  examination_name text,
-  done BOOLEAN,
-  user_id int references UserInfo(id)
-);
+
 ------------------------------------------------------------------------------------------------------------
 create table Question(
     id int primary key,
-    question text,
     question text,
     user_id int references UserInfo(id),
     category_id int references Question_category(id)
