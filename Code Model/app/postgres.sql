17c96cac5cd09d7c2b4edf6bdde6fc0c241ceb43
JosiahRegencia <jawshaeleazar@gmail.com>
2016-02-25 00:17:44 +0800

    added tables to postgre.sql

--- Code Model/app/postgres.sql
+++ Code Model/app/postgres.sql
@@ -9,3 +9,44 @@ create table User (
 
 ----------------------------------------------------------------------------------------------------
 
+create table Symptom(
+  id int primary key,
+  symptom varchar(200),
+);
+
+-----------------------------------------------------------------------------------------------------
+
+CREATE TABLE Schedule(
+  id int primary key,
+  date_time_year date,
+  done boolean
+);
+
+create or replace newschedule(par_id int, par_date_time_year date, par_done BOOLEAN) return text as
+$$
+    declare
+        loc_id TEXT;
+        loc_res TEXT;
+    begin
+        select into loc_id id Schedule where id = par_id;
+
+        if loc_id isnull then
+            insert into Schedule(id, date_time_year, done) values (par_id, par_date_time_year, par_done);
+            loc_res = 'OK';
+
+        else
+            loc_res = 'ID EXISTED';
+        end if
+        return loc_res
+    end;
+$$
+    language 'plpgsql';
+
+-----------------------------------------------------------------------------------------------------------
+
+create table Question_category (
+    id int primary key,
+    category varchar(100)
+);
+
+------------------------------------------------------------------------------------------------------------
