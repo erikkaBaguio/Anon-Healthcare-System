646c3db5458a54cadf17fc22fe11fb2254e359b1
JosiahRegencia <jawshaeleazar@gmail.com>
2016-02-25 05:38:18 +0800

    Initialized temporary.js

--- Code Model/app/templates/temporaray.js
+++ Code Model/app/templates/temporaray.js
@@ -3,7 +3,22 @@
  */
 function newuser(fname, mname, lname, email, password, role, is_active) {
      return '<div class="col-lg-12">' +
-             <div class="form-group"
-             '<h4>' + id + "&nbsp;&nbsp;" + title + '</h4>' +
-             '<p>'+description+' </br> Status: '+done+'</p> </div>';
+             '<div class="form-group">' +
+             '<label class="col-md-2 control-label">First Name</label>' +
+             '<div class="col-md-10"> + <input name="fname" type="text" class="form-control" </div></div>';
+             //'<h4>' + id + "&nbsp;&nbsp;" + title + '</h4>' +
+             //'<p>'+description+' </br> Status: '+done+'</p> </div>';
+}
+
+function adduser() {
+
+    $.ajax({
+        url: 'http://127.0.0.1:5000/adduser',
+        type: "POST",
+        dataType: "json",
+        success: function(resp) {
+
+        }
+        error:
+    });
 }
