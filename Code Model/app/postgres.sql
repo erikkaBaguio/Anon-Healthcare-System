c209531123b5597c02ad7b5932c9b8c73c750838
erikkaBaguio <erikka.baguio@gmail.com>
2016-02-25 00:47:55 +0800

    Added tables in postgres.sql (disease_symptom, examination, question,question_answer)

--- Code Model/app/postgres.sql
+++ Code Model/app/postgres.sql
@@ -50,10 +50,40 @@ create table Question_category (
 );
 
 ------------------------------------------------------------------------------------------------------------
-create table disease(
+create table Disease(
   id int primary key,
   name varchar(200),
   done boolean,
 );
 
 ------------------------------------------------------------------------------------------------------------
+create table Disease_Symptom(
+  id int primary key,
+  disease_id int references Disease(id),
+  symptom_id int references Symptom(id),
+  user_id int references User(id),
+);
+------------------------------------------------------------------------------------------------------------
+create table Examination (
+  id int primary key,
+  user_id int references User(id),
+  schedule_id int references Schedule(id),
+  question_id int references Question(id),
+  answer_id int references Answer(id),
+  examination_name varchar(200)
+  done BOOLEAN
+);
+------------------------------------------------------------------------------------------------------------
+create table Question(
+    id int primary key,
+    question varchar (200),
+    user_id int references User(id),
+    category_id int references Question_category(id)
+);
+------------------------------------------------------------------------------------------------------------
+create table Question_answer (
+    id int primary key,
+    question_id int references Question(id),
+    answer_id int references  Answer(id),
+    chained_to_question int references  Question(question)
+);
