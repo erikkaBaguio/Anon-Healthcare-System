5717461749b1d8e7c856dbd0449d0e539c16c4a3
marygracecabolbol <marygrace.cabolbol@g.msuiit.edu.ph>
2016-02-25 06:19:33 +0800

    updated the table Disease symptom and Question category in postgres.sql

--- Code Model/app/postgres.sql
+++ Code Model/app/postgres.sql
@@ -154,8 +154,30 @@ create table Disease_Symptom(
   id int primary key,
   disease_id int references Disease(id),
   symptom_id int references Symptom(id),
-  user_id int references UserInfo(id)
+  user_id int references UserInfo(id),
+  done BOOLEAN
 );
+
+CREATE  or replace function newdisease_symptom(par_id int, par_disease_id int, par_symptom_id int, par_user_id int, par_done boolean) returns text AS
+$$
+  DECLARE
+    loc_id text;
+    loc_res text;
+
+  BEGIN
+    SELECT INTO loc_id id FROM Disease_Symptom where id = par_id;
+    if loc_id isnull THEN
+
+      INSERT INTO Disease_Symptom(id, disease_id, symptom_id, user_id, done) VALUES (par_id, par_disease_id, par_symptom_id, par_user_id, par_done);
+      loc_res = 'OK';
+
+    else
+       loc_res = 'ID EXISTED';
+    end if;
+    return loc_res;
+  end;
+$$
+ language 'plpgsql';
 
 ------------------------------------------------------------------------------------------------------------
 create table Question(
@@ -195,4 +217,22 @@ create table Question_answer (
     done BOOLEAN
 );
 
+CREATE or replace function newquestion_answer(par_id int, par_question_id int, par_answer_id int, par_chained_to_question int, done boolean) returns text as
+$$
+  declare
+    loc_id text;
+    loc_res text;
+  begin
+     select into loc_id id from Question_answer where id = par_id;
+     if loc_id isnull then
+       insert into Question_answer (id, question_id, answer_id, chained_to_question, done) values (par_id, par_question_id, par_done);
+       loc_res = 'OK';
+
+     else
+       loc_res = 'ID EXISTED';
+     end if;
+     return loc_res;
+  end;
+$$
+ language 'plpgsql';
 ---------------------------------------------------------------------------------------------------------------
