2625a6e425a958d6d9d49f47d6fd242e5bcb7df0
JosiahRegencia <jawshaeleazar@gmail.com>
2016-02-25 05:22:09 +0800

    Edited temporary.html

---
+++ Code Model/app/templates/temporaray.js
@@ -0,0 +1,9 @@
+/**
+ * Created by josiah on 2/25/16.
+ */
+function newuser(fname, mname, lname, email, password, role, is_active) {
+     return '<div class="col-lg-12">' +
+             <div class="form-group"
+             '<h4>' + id + "&nbsp;&nbsp;" + title + '</h4>' +
+             '<p>'+description+' </br> Status: '+done+'</p> </div>';
+}
