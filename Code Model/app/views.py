f8397e3fc4df7f4af73780dc46e9518041ff8ed4
JosiahRegencia <jawshaeleazar@gmail.com>
2016-02-25 17:54:35 +0800

    Added script source for temporary.js

--- Code Model/app/views.py
+++ Code Model/app/views.py
@@ -49,7 +49,7 @@ def login():
     flash('Logged in successfully')
     return redirect(request.args.get('next') or url_for('index'))
 
-@app.route('/addUser', methods=['GET'])
+@app.route('/addUser', methods=['POST'])
 def addUser(fname):
 
     res = spcall()
