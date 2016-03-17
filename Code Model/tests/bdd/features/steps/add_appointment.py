038788590d75a2c0de89dea79e06e4a71b71f72f
remarcbalisi <remarcbalisi@gmail.com>
2016-03-17 09:11:14 +0800

    import APPOINTMENTS

--- Code Model/tests/bdd/features/steps/add_appointment.py
+++ Code Model/tests/bdd/features/steps/add_appointment.py
@@ -4,7 +4,7 @@ from lettuce import step, world, before
 from nose.tools import assert_equals
 
 from app import app
-from app.views import APPOINTMENTS
+import app.views import APPOINTMENTS
 
 
 @before.all
@@ -28,19 +28,3 @@ def and_the_following_disease_details_are_returned(step):
     assert_equals(step.hashes, [json.loads(world.response.data)])
 
 # =====================================================================================
-
-@step(u'Given disease records that are already stored in the system')
-def given_disease_records_that_are_already_stored_in_the_system(step):
-    DISEASE_RECORDS.update({"id": '1', "disease_id": '1', "symptom_id": '3', "done": 'True'})
-
-@step(u'When the admin retrieve the disease record with an id number \'([^\']*)\'')
-def when_the_admin_retrieve_the_disease_record_with_an_id_number_group1(step, id):
-    world.response = world.app.get('/anoncare.api/disease_records/{}/'.format(id))
-
-@step(u'Then I should get a \'([^\']*)\' response')
-def then_i_should_get_a_group1_response(step, expected_status_code):
-    assert_equals(world.response.status_code, int(expected_status_code))
-
-@step(u'And the following disease record details are returned:')
-def and_the_following_disease_record_details_are_returned(step):
-    assert_equals(step.hashes, [json.loads(world.response.data)])
