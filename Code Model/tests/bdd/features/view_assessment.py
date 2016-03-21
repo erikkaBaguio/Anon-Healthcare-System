1367e4e03403f80bb1da50551234886c698e5f8c
erikkaBaguio <erikka.baguio@gmail.com>
2016-03-21 14:46:00 +0800

    Added view assessment steps for tetsing.

--- Code Model/tests/bdd/features/view_assessment.py
+++ Code Model/tests/bdd/features/view_assessment.py
@@ -5,6 +5,7 @@ from nose.tools import assert_equals, assert_raises, raises
 from flask import *
 from app import app
 
+
 @step(u'Given   the patient assessment with an id \'([^\']*)\' exists')
 def given_the_patient_assessment_with_an_id_group1_exists(step, assessment_id):
     world.assessment = world.app.get('/anoncare.api/assessments/1/')
@@ -15,12 +16,17 @@ def given_the_patient_assessment_with_an_id_group1_exists(step, assessment_id):
 def when_the_doctor_press_view_assessment_with_an_id_1(step):
     world.response = world.app.get('/anoncare.api/assessments/1/')
 
-@step(u'Then    the doctor should get a \'([^\']*)\' response')
-def then_the_doctor_should_get_a_group1_response(step, expected_status_code):
+@step("the doctor should get a \'([^\']*)\' response")
+def step_impl(step,expected_status_code):
+    """
+    :type step: lettuce.core.Step
+    """
     assert_equals(world.response.status_code, int(expected_status_code))
 
-@step(u'And     the following assessment details will be returned')
-def and_the_following_assessment_details_will_be_returned(step):
+@step("the following assessment details will be returned")
+def step_impl(step):
+    """
+    :type step: lettuce.core.Step
+    """
     resp = json.loads(world.response.data)
     assert_equals(world.resp['entries'], resp['entries'])
-
