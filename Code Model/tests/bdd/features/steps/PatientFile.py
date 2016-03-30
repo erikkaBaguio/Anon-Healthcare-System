418aa9e11366e811103049e529768562337564c7
marygracecabolbol <marygrace.cabolbol@g.msuiit.edu.ph>
2016-03-31 05:17:15 +0800

    edited the steps for getting the patient file in PatientFile.py

--- Code Model/tests/bdd/features/steps/PatientFile.py
+++ Code Model/tests/bdd/features/steps/PatientFile.py
@@ -35,6 +35,22 @@ def and_i_should_get_a_message_OK(step):
 
 #-----------------------------------------------------------------------------------------------------
 
+@step(u'Given the following patient file already exists')
+def given_the_following_patient_file_already_exists(step):
+    world.patient = step.hashes[0]
+
+@step(u'When I retrieve patient id \'([^\']*)\'')
+def when_I_retrieve_patient_id_1(step, id):
+    world.response = world.app.get('/anoncare.api/patient/{}/'.format(id))
+
+@step(u'Then I get a \'([^\']*)\' response')
+def then_i_get_a_200_response(step, expected_status_code):
+    assert_equals(world.response.status_code, int(expected_status_code))
+
+@step(u'And I can retrieve the patient file.')
+def and_i_can_retrieve_the_patient_file(step):
+    world.resp = json.loads(world.response.data)
+    assert_equals(world.resp['status'], 'OK')
 
 
 
