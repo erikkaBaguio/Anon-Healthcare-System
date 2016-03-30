f8b24d7c3f85cedb57557cae89e40d70a19b4536
marygracecabolbol <marygrace.cabolbol@g.msuiit.edu.ph>
2016-03-30 14:20:25 +0800

    removed the POST api for personal and pulmonary, and GET api for personal

--- Code Model/app/views.py
+++ Code Model/app/views.py
@@ -260,36 +260,7 @@ def newpatient():
 
     return jsonify({'status': 'OK', 'message': response[0][0]}), 200
 
-@app.route('/anoncare.api/patient/personal/', methods = ['POST'])
-def newpersonal():
-    data = json.loads(request.data)
-    response = spcall('newpersonal_info', (
-        data['height'],
-        data['weight'],
-        data['date_of_birth'],
-        data['civil_status'],
-        data['name_of_guardian'],
-        data['home_address'],
-        data['is_active'],),True)
-    
-    if 'Error' in str(response[0][0]):
-        return jsonify({'status': 'error', 'message': response[0][0]})
 
-    return jsonify({'status': 'OK', 'message': response[0][0]}), 200
-
-@app.route('/anoncare.api/patient/pulmonary/', methods = ['POST'])
-def newpulmonary():
-    data = json.loads(request.data)
-    response = ('newpulmonary', (
-        data['cough'],
-        data['dyspnea'],
-        data['hemoptysis'],
-        data['tb_exposure'],),True)
-
-    if 'Error' in str(response[0][0]):
-        return jsonify({'status': 'error', 'message': response[0][0]})
-
-    return jsonify({'status': 'OK', 'message': response[0][0]}), 200
 
 @app.route('/anoncare.api/patient/<id>/', methods = ['GET'])
 def getpatient_file(id):
@@ -310,31 +281,7 @@ def getpatient_file(id):
                         "personal_info_id": row[7],
                         "is_active": row[8]})
         return jsonify({"status": "OK", "message": "OK", "entries": entries, "count":len(entries)})
-
-
-@app.route('/anoncare.api/patient/personal/<id>/', methods =['GET'])
-def get_allpatient(id):
-    response = spcall('get_patientID', id)
-    entries = []
-    if len(response) == 0:
-        return jsonify({"status":"OK", "message": "No patient file found", "entries":[], "count": "0"})
-    else:
-        row = response[0]
-        entries.append({"id": id,
-                        "fname": row[0],
-                        "mname": row[1],
-                        "lname": row[2],
-                        "age": row[3],
-                        "sex": row[4],
-                        "height": row[5],
-                        "weight": row[6],
-                        "date_of_birth": row[7],
-                        "civil_status": row[8],
-                        "name_of_guardian": row[9],
-                        "home_address": row[10]
-                        })
-        return jsonify({"status":"OK", "message":"OK", "entries": entries, "count": len(entries)})
-
+        
 
 @app.route('/anoncare.api/notify/<int:assessment_id>/<int:doctor_id>', methods=['GET'])
 def getnotify(assessment_id, doctor_id):
