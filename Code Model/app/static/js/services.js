756e9875b1825d6636d636d3f484c10c129fc3f8
erikkaBaguio <erikka.baguio@gmail.com>
2016-04-07 15:50:56 +0800

    Modified services.js for authentication

--- Code Model/app/static/js/services.js
+++ Code Model/app/static/js/services.js
@@ -3,3 +3,19 @@
  * Services - API caller
  */
 
+angular.module('myApp').factory('AuthService',
+  ['$q', '$timeout', '$http',
+  function ($q, $timeout, $http) {
+
+    // create user variable
+    var user = null;
+
+    // return available functions for use in controllers
+    return ({
+      isLoggedIn: isLoggedIn,
+      login: login,
+      logout: logout,
+      register: register
+    });
+
+}]);
