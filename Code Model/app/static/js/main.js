540b86317b6cb6e7dab1885393f57a9fbafce5ca
erikkaBaguio <erikka.baguio@gmail.com>
2016-04-07 09:24:45 +0800

    Modified main.js and index.html

--- Code Model/app/static/js/main.js
+++ Code Model/app/static/js/main.js
@@ -2,7 +2,7 @@
  * Created by Erikka on 4/7/2016.
  */
 
-var myApp = angular.module("myApp",['ngRoute']);
+var myApp = angular.module("myApp",[]);
 
 myApp.config(function ($routeProvider) {
     $routeProvider
@@ -15,4 +15,16 @@ myApp.config(function ($routeProvider) {
       controller: 'loginController',
       access: {restricted: false}
     })
+});
+
+myApp.run(function ($rootScope, $location, $route, AuthService) {
+  $rootScope.$on('$routeChangeStart',
+    function (event, next, current) {
+      AuthService.getUserStatus();
+      if (next.access.restricted &&
+          !AuthService.isLoggedIn()) {
+        $location.path('/login');
+        $route.reload();
+      }
+  });
 });
