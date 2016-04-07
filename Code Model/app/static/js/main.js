387bb890a9a4071c8517390d860d4059bfc1de6f
erikkaBaguio <erikka.baguio@gmail.com>
2016-04-07 09:00:30 +0800

    Added controller.js and modified main.js and index.html

--- Code Model/app/static/js/main.js
+++ Code Model/app/static/js/main.js
@@ -2,4 +2,17 @@
  * Created by Erikka on 4/7/2016.
  */
 
-var myApp = angular.module("mainApp",['ngRoute']);
+var myApp = angular.module("myApp",['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+      templateUrl: 'static/index.html',
+      access: {restricted: true}
+    })
+    .when('/login', {
+      templateUrl: 'static/pages/login.html',
+      controller: 'loginController',
+      access: {restricted: false}
+    })
+});
