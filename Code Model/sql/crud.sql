ce9eb6429f672c88ed5c36918e5173fbdb1b4976
erikkaBaguio <erikka.baguio@gmail.com>
2016-05-02 08:50:32 +0800

    Added stored proc functions for adding, updating, retrieving assessment

--- Code Model/sql/crud.sql
+++ Code Model/sql/crud.sql
@@ -113,3 +113,191 @@ $$
  language 'plpgsql';
 
 ------------------------------------------------------------- END USER -------------------------------------------------------------
+
+------------------------------------------------------------ ASSESSMENT ------------------------------------------------------------
+-- [POST] Create new assessment
+-- select store_assessment(1,20130000, 37.1, 80, 19, '90/70', 48, 'complaint', 'history', 'medication1', 'diagnosis1','recommendation1', 1);
+-- select store_assessment(2,20130001, 36.4, 70, 19, '100/80', 45, 'complaint', 'history', 'medication1', 'diagnosis1','recommendation1', 1);
+CREATE OR REPLACE FUNCTION store_assessment(IN par_id                       INT,
+                                          IN par_schoolID                 INT,
+                                          IN par_temperature              FLOAT,
+                                          IN par_pulse_rate               FLOAT,
+                                          IN par_respiration_rate         INT,
+                                          IN par_blood_pressure           TEXT,
+                                          IN par_weight                   FLOAT,
+                                          IN par_chiefcomplaint           TEXT,
+                                          IN par_historyofpresentillness  TEXT,
+                                          IN par_medicationstaken         TEXT,
+                                          IN par_diagnosis                TEXT,
+                                          IN par_recommendation           TEXT,
+                                          IN par_attendingphysician       INT)
+  RETURNS TEXT AS
+$$
+DECLARE
+  loc_id1       INT;
+  loc_id2       INT;
+  loc_res       TEXT;
+BEGIN
+  SELECT INTO loc_id1 id
+  FROM Assessment
+  WHERE id = par_id;
+
+  SELECT INTO loc_id2 id
+  FROM Vital_signs
+  WHERE id = par_id;
+
+  IF loc_id1 ISNULL AND loc_id2 ISNULL
+  THEN
+    IF par_chiefcomplaint = '' OR
+       par_chiefcomplaint ISNULL OR
+       par_medicationstaken = '' OR
+       par_medicationstaken ISNULL OR
+       par_diagnosis = '' OR
+       par_diagnosis ISNULL
+    THEN
+      loc_res = 'PLEASE FILL THE REQUIRE FIELDS';
+
+    ELSE
+        INSERT INTO Vital_signs(id, temperature, pulse_rate, respiration_rate, blood_pressure, weight)
+        VALUES (par_id,par_temperature, par_pulse_rate, par_respiration_rate, par_blood_pressure, par_weight);
+
+        INSERT INTO Assessment (id,school_id, vital_signsID, chiefcomplaint, historyofpresentillness,
+                                medicationstaken, diagnosis, recommendation, attendingphysician)
+        VALUES (par_id,par_schoolID, par_id, par_chiefcomplaint, par_historyofpresentillness, par_medicationstaken,
+                par_diagnosis, par_recommendation, par_attendingphysician);
+
+        loc_res = 'OK';
+
+    END IF;
+  ELSE
+      loc_res = 'ID EXISTS';
+  END IF;
+  RETURN loc_res;
+
+END;
+$$
+  LANGUAGE 'plpgsql';
+
+
+--[GET] Retrieve assessment of a specific patient
+--select show_assessment(20130000,1);
+CREATE OR REPLACE FUNCTION show_assessment(IN par_schoolID INT,
+                                           IN par_id INT,
+                                           OUT INT,
+                                           OUT TIMESTAMP,
+                                           OUT INT,
+                                           OUT INT,
+                                           OUT TEXT,
+                                           OUT TEXT,
+                                           OUT TEXT,
+                                           OUT TEXT,
+                                           OUT TEXT,
+                                           OUT INT,
+                                           OUT BOOLEAN,
+                                           OUT FLOAT,
+                                           OUT FLOAT,
+                                           OUT INT,
+                                           OUT TEXT,
+                                           OUT FLOAT,
+                                           OUT TEXT,
+                                           OUT TEXT)
+  RETURNS SETOF RECORD AS
+$$
+
+select Assessment.*,
+         Vital_signs.temperature,
+         Vital_signs.pulse_rate,
+         Vital_signs.respiration_rate,
+         Vital_signs.blood_pressure,
+         Vital_signs.weight,
+         Userinfo.fname,
+         Userinfo.lname
+  FROM Assessment
+  INNER JOIN Vital_signs ON (
+    Assessment.vital_signsID = Vital_signs.id
+    )
+  INNER JOIN Userinfo ON (
+    Assessment.attendingphysician = Userinfo.id
+    )
+  WHERE Assessment.id = par_id
+  AND Assessment.school_id = par_schoolID
+
+$$
+  LANGUAGE 'sql';
+
+
+-- [GET] Retrieve all assessment of a specific patient
+--select show_allassessment(20130000);
+CREATE OR REPLACE FUNCTION show_allassessment(IN par_schoolID INT,
+                                           OUT INT,
+                                           OUT TIMESTAMP,
+                                           OUT INT,
+                                           OUT INT,
+                                           OUT TEXT,
+                                           OUT TEXT,
+                                           OUT TEXT,
+                                           OUT TEXT,
+                                           OUT TEXT,
+                                           OUT INT,
+                                           OUT BOOLEAN,
+                                           OUT FLOAT,
+                                           OUT FLOAT,
+                                           OUT INT,
+                                           OUT TEXT,
+                                           OUT FLOAT,
+                                           OUT TEXT,
+                                           OUT TEXT)
+  RETURNS SETOF RECORD AS
+$$
+  SELECT Assessment.*,
+         Vital_signs.temperature,
+         Vital_signs.pulse_rate,
+         Vital_signs.respiration_rate,
+         Vital_signs.blood_pressure,
+         Vital_signs.weight,
+         Userinfo.fname,
+         Userinfo.lname
+  FROM Assessment
+  INNER JOIN Vital_signs ON (
+    Assessment.vital_signsID = Vital_signs.id
+    )
+  INNER JOIN Userinfo ON (
+    Assessment.attendingphysician = Userinfo.id
+    )
+  WHERE Assessment.school_id = par_schoolID
+  ORDER BY id DESC;
+
+$$
+  LANGUAGE 'sql';
+
+
+--[PUT] Update assessment of patient
+--select update_assessment(1,20130000, 'medication1f', 'diagnosis11f','recommendation11', 1);
+CREATE OR REPLACE FUNCTION update_assessment(IN par_id                 INT,
+                                             IN par_schoolID           TEXT,
+                                             IN par_medicationstaken   TEXT,
+                                             IN par_diagnosis          TEXT,
+                                             IN par_recommendation     TEXT,
+                                             IN par_attendingphysician INT)
+  RETURNS TEXT AS
+$$
+DECLARE
+  loc_res TEXT;
+BEGIN
+
+  UPDATE Assessment
+  SET
+    diagnosis          = par_diagnosis,
+    recommendation     = par_recommendation,
+    attendingphysician = par_attendingphysician
+  WHERE id = par_id
+  AND school_id = par_schoolID;
+
+  loc_res = 'Updated';
+  RETURN loc_res;
+
+END;
+$$
+  LANGUAGE 'plpgsql';
+
+---------------------------------------------------------- END ASSESSMENT ----------------------------------------------------------
