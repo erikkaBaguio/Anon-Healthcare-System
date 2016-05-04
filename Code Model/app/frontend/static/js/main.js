a6c53235d2c050b272237b1ede88598a72835f25
erikkaBaguio <erikka.baguio@gmail.com>
2016-05-04 13:18:55 +0800

    Modified main.js for routing

--- Code Model/app/frontend/static/js/main.js
+++ Code Model/app/frontend/static/js/main.js
@@ -3,25 +3,57 @@
  * View - routing
  */
 
-var myApp = angular.module("myApp", ["ngRoute"]);
 
-myApp.config(function ($routeProvider) {
+/*Main Module*/
+var myApp = angular.module("myApp", ['ngRoute', 'ngResource']);
+
+myApp.config(function ($routeProvider, $resourceProvider, $locationProvider) {
+
     $routeProvider
-    .when('/', {
-      templateUrl: 'pages/doctor/dashboard-doctor.html',
-      controller: 'dashboardController',
-      access: {restricted: false}
-    })
-    .when('/dashboard',{
-        templateUrl: 'pages/dashboard-nurse.html',
-        access: {restricted:true}
-    })
-    .when('/logout', {
-        controller: 'logoutController',
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
+
+        /*Authentication*/
+        .when('/login',{
+            templateUrl: 'pages/login.html',
+            controller: 'dashboardController'
+        })
+
+        .when('/logout', {
+            controller: 'logoutController'
+        })
+
+
+        /*Assessment*/
+        .when('/dashboard',{
+            templateUrl: 'pages/nurse/dashboard-nurse.html',
+            controller: 'dashboardController'
+        })
+
+        .when('/dashboard/assessments/add',{
+            templateUrl: 'pages/nurse/dashboard-nurse.html',
+            controller: 'assessmentController'
+        })
+
+        .when('/dashboard/assessments/:id',{
+            templateUrl: 'pages/nurse/dashboard-nurse.html',
+            controller: 'assessmentController'
+        })
+
+        /*Patients*/
+        .when('/dashboard/patients/add',{
+            templateUrl: 'pages/nurse/dashboard-nurse.html',
+            controller: 'patientsController'
+        })
+
+        .when('/dashboard/patients/:id',{
+            templateUrl: 'pages/nurse/dashboard-nurse.html',
+            controller: 'patientsController'
+        })
+
+        .otherwise({
+          redirectTo: '/dashboard'
+        });
+
+    $resourceProvider.defaults.stripTrailingSlashes = false;
 });
 
 // Change of state
