da1631d50de93727cda179f8709614dc7fc8e813
erikkaBaguio <erikka.baguio@gmail.com>
2016-05-05 19:20:32 +0800

    Modified main.js and index template

--- Code Model/app/frontend/static/js/main.js
+++ Code Model/app/frontend/static/js/main.js
@@ -8,32 +8,36 @@
  *Main Module of The Application
  */
 
-var myApp = angular.module("myApp", ['ui.router', 'ngResource']);
+var myApp = angular.module('myApp', ['ui.router']);
 
 myApp.config(function ($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.when('/dashboard');
-    $urlRouterProvider.otherwise('/login');
+    $urlRouterProvider.otherwise('/dashboard');
 
     $stateProvider
         .state('base', {
             abstract: true,
             url: '',
-            templateUrl: ''
+            templateUrl: 'pages/base.html'
         })
 
         /*Authentication*/
         .state('login', {
             url: '/login',
-            parent: 'base',
             templateUrl: 'pages/login.html',
             controller: 'loginController'
         })
 
         .state('dashboard', {
-          url: '/dashboard',
-          parent: 'base',
-          templateUrl: 'pages/dashboard.html',
-          controller: 'DashboardCtrl'
+            url: '/dashboard',
+            parent: 'base',
+            templateUrl: 'pages/dashboard.html',
+            controller: 'loginController'
+        })
+        
+        .state('assessment-add', {
+            url: '/dashboard',
+            templateUrl: 'pages/nurse/assessment.html',
+            controller: 'loginController'
         })
 });
