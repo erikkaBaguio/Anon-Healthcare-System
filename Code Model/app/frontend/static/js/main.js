6e72a3cdcbb9394fb827fc7c38be4d1ccd9b4af3
erikkaBaguio <erikka.baguio@gmail.com>
2016-05-06 08:54:28 +0800

    Updated template for searching assessment, main.js, and dashboard template

--- Code Model/app/frontend/static/js/main.js
+++ Code Model/app/frontend/static/js/main.js
@@ -36,8 +36,15 @@ myApp.config(function ($stateProvider, $urlRouterProvider) {
                 },
             }
         })
+
         .state('dashboard.assessment-add', {
             url: '/assessments/add ',
             templateUrl: 'pages/content/assessment.html'
         })
+
+        .state('dashboard.patient-search', {
+            url: '/patients ',
+            templateUrl: 'pages/content/assessments-search.html'
+        })
+
 });
